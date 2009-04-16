0b188b3b93656d7cfd467677409d1ff35c1c3c4e
Jessica Tsuji <jessicat@Jessica-Tsujis-PowerBook.local>
2009-04-15 21:35:26 -0700

    Changed head

--- index.php
+++ index.php
@@ -9,9 +9,9 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
-	<title>Blend</title>
+	<title>AddressBook App</title>
 	<link type="text/css" rel="stylesheet" href="css/mainStyles.css" media="all" />
-	<script type="text/javascript" src="scripts/jquery.js"></script>
+	<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
 	<script type="text/javascript" src="scripts/jqueryUI.js"></script>
 	<script type="text/javascript" src="scripts/signIn.js"></script>
 	<script type="text/javascript">
