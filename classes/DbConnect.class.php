c7670d57a0f2a2c5356d347be95caa15ac1d669f
Kellan Craddock <kellan@Kellan-Craddocks-PowerBook-2.local>
2009-04-15 22:34:21 -0700

    Added DbConnect class and Login class.

---
+++ classes/DbConnect.class.php
@@ -0,0 +1,45 @@
+<?php
+
+class DbConnect {
+	private $host = 'localhost';
+	private $user = 'adelinei_admin';
+	private $pass = 'admin';
+	private $name = 'adelinei_login';
+	private $conn;
+	
+	private static $instance;
+	
+	private function __construct() {
+		//Connect 
+		mysql_connect($this->host, $this->user, $this->pass) or die (mysql_error());
+		//Select DB
+		mysql_select_db($this->name) or die('could not select' . mysql_error());
+		
+	}
+	
+	public static function get() {
+		
+		if(!isset(self::$instance)) {
+			self::$instance = new DbConnect;
+		}
+		
+		return self::$instance;
+	}
+	
+	public function query($string, $arguments) {
+		
+		foreach($arguments as &$argument) {
+			$argument = mysql_real_escape_string($argument);
+			//echo($argument);
+		}
+		
+		$statement = vsprintf($string, $arguments);
+		//echo($statement);
+		$result = mysql_query($statement) or die(mysql_error());
+		
+		return $result;
+	}
+	
+}
+
+?>
