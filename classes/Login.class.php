ae65618ecea145e13a31d3b7410a7c027a293a5d
Kellan Craddock <kellan@Kellan-Craddocks-PowerBook-2.local>
2009-04-15 23:37:16 -0700

    Login class in now working.

--- classes/Login.class.php
+++ classes/Login.class.php
@@ -13,8 +13,7 @@ class Login {
 	private static $errorMsg;
 	
 	public function __construct() {
-		return true;
-
+		
 		$this->user_name = $_POST['userName'];
 		$this->password = $_POST['password'];
 		$this->table = "users";
@@ -70,8 +69,9 @@ class Login {
 	private function handleResults() {
 		//split results into array
 		$this->line = mysql_fetch_array($this->result, MYSQL_ASSOC);
-
+		
 		if ($this->user_name == $this->line['user_name'] && $this->password == $this->line['password']) {
+		
 			//Set session redirect
 			$_SESSION['login'] = true;
 			$_SESSION['current_user'] = $this->line['user_name'];
@@ -91,56 +91,4 @@ class Login {
 	
 	
 }
-	//Start Session
-	//session_start();
-	/*
-	//DB Connect
-	include("../classes/dbconnect.class.php");
-	
-	//Check if username and password are sent
-	if (isset($_POST['userName']) && isset($_POST['password'])) {
-	
-		//Encrypt user input for password
-		$crypt_password = crypt($_POST['password'], md5($_POST['userName']));
-		
-		//Use a clean SELECT stament to grab user info
-		$query = sprintf("SELECT * FROM %s WHERE user_name = '%s' AND password = '%s'",
-			mysql_real_escape_string('users'),
-			mysql_real_escape_string($_POST['userName']),
-			mysql_real_escape_string($crypt_password)
-		);
-		
-		//Get query result
-		$result = mysql_query($query) or die('could not query' . mysql_error());
-		
-		//Turn result into associative array
-		$line = mysql_fetch_array($result, MYSQL_ASSOC);
-		
-		//Set error variable
-		$error = '';
-		
-		//If username and password match, set session and redirect, else error
-		if ($_POST['userName'] == $line['user_name'] && $crypt_password == $line['password']) {
-			//Set session redirect
-			$_SESSION['login'] = true;
-			$_SESSION['current_user'] = $line['user_name'];
-			
-			//Update logged_in status.
-			$update_query = sprintf("UPDATE %s SET logged_in = 1 WHERE user_name = '%s' AND password = '%s'",
-				mysql_real_escape_string('users'),
-				mysql_real_escape_string($_POST['userName']),
-				mysql_real_escape_string($crypt_password)
-			);
-			//Query the Update
-			$update_result = mysql_query($update_query) or die('could not query' . mysql_error());
-			
-			header("Location: ../home.php");
-		} else {
-			$error .= 'Bad username and password';
-			
-			$_SESSION['error'] = $error;
-			header("Location: ../index.php");
-		}
-	}
-	*/
 ?>
