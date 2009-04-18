6852fbf02ac3889eb988635aa36b621ad6af8ad6
Jessica Tsuji <jessicat@Jessica-Tsujis-PowerBook.local>
2009-04-17 22:34:41 -0700

    Commit Conflict Merge

--- classes/InterfaceActions.class.js
+++ classes/InterfaceActions.class.js
@@ -1,11 +1,13 @@
 function InterfaceActions() {
 	var self = this;
 	this.root;
+	//Create new instance of ambassador class here
 	
 	this.ajaxAmbassador = new AjaxAmbassador();
 	this.processing = new Processing();
 	
 	this.construct = function(element) {
+		//construct ambassador class
 		self.root = element;
 		$('#addContact form').submit(function() {
 			self.addContact(self.root);
@@ -22,5 +24,15 @@ function InterfaceActions() {
 		self.ajaxAmbassador.construct(element);
 		self.processing.construct(element);
 	
+	this.addContact = function() {
+		//Wait for event
+			//ambassador.makeRequest(data)
+			//Do something with returned data
+	}
+	
+	this.editContact = function() {
+		//Wait for event
+			//ambassador.makeRequest(data)
+			//Do something with returned data
 	}
 }
