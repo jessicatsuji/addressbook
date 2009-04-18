b5250c91925ad5536291ac742e784d4d9c4d25ad
Kellan Craddock <kellan@Kellan-Craddocks-PowerBook-2.local>
2009-04-17 22:31:48 -0700

    Added comments

--- classes/InterfaceActions.class.js
+++ classes/InterfaceActions.class.js
@@ -1,16 +1,22 @@
 function InterfaceActions() {
 	var self = this;
 	this.root;
+	//Create new instance of ambassador class here
 	
 	this.construct = function(element) {
+		//construct ambassador class
 		self.root = element;
 	}
 	
 	this.addContact = function() {
-	
+		//Wait for event
+			//ambassador.makeRequest(data)
+			//Do something with returned data
 	}
 	
 	this.editContact = function() {
-	
+		//Wait for event
+			//ambassador.makeRequest(data)
+			//Do something with returned data
 	}
 }
