5cec461df119e8c2d1ca9fd5c8baa28e6d2c11d7
Jessica Tsuji <jessicat@Jessica-Tsujis-PowerBook.local>
2009-04-17 21:11:48 -0700

    Created InterfaceActions class to handle adding & editing contacts

---
+++ classes/InterfaceActions.class.js
@@ -0,0 +1,16 @@
+function InterfaceActions() {
+	var self = this;
+	this.root;
+	
+	this.construct = function(element) {
+		self.root = element;
+	}
+	
+	this.addContact = function() {
+	
+	}
+	
+	this.editContact = function() {
+	
+	}
+}
