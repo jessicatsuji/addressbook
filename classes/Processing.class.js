51e9e3c8d95fb798fa3af2a2c65a69c5a820f4ed
Jessica Tsuji <jessicat@Jessica-Tsujis-PowerBook.local>
2009-04-17 21:12:27 -0700

    Created Processing class to handle JSON data for adding & editing

---
+++ classes/Processing.class.js
@@ -0,0 +1,16 @@
+function Processing() {
+	var self = this;
+	this.root;
+	
+	this.construct = function(element) {
+		self.root = element;
+	}
+	
+	this.add = function() {
+	
+	}
+	
+	this.edit = function() {
+	
+	}
+}
