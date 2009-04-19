4c03a9ee6a44308700123ec6ecd62b0442c6146a
Kellan Craddock <kellan@Kellan-Craddocks-PowerBook-2.local>
2009-04-19 14:20:37 -0700

    Set up returnArray and echoed back encoded json data

--- scripts/addContact.php
+++ scripts/addContact.php
@@ -1,6 +1,11 @@
 <?php
 	session_start();
-	require_once("../includes/config.inc.php");
+	require_once("../classes/AddContact.class.php");
 	require_once("../classes/DbConnect.class.php");
-	new AddContact;
+	
+	$addContact = new AddContact();
+	$retunArray = $addContact->return_array;
+	//$returnArray = array("id"=>1, "message"=>"error message");
+	$data = json_encode($returnArray);
+	echo $data;
 ?>
