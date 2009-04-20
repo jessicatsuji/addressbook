86032b03e7c4254dbe2cd0c16f375aaefb1e5b77
Gerardo Rodriguez <chocho@chocho.local>
2009-04-19 21:40:29 -0700

    added the data into the input and textarea field from the db

--- classes/Processing.class.js
+++ classes/Processing.class.js
@@ -19,34 +19,34 @@ function Processing() {
 			content += 		'<form action="#" method="post">';
 			content += 			'<fieldset>';
 			content += 				'<label>First Name:</label>';
-			content += 				'<input type="text" name="firstName' + returnData['data']['id'] + '" id="firstName' + returnData['data']['id'] + '" />';
+			content += 				'<input type="text" name="firstName' + returnData['data']['id'] + '" id="firstName' + returnData['data']['id'] + '" value="' + returnData['data']['first_name'] + '" />';
 			
 			content += 				'<label>Last Name:</label>';
-			content +=				'<input type="text" name="lastName' + returnData['data']['id'] + '" id="lastName' + returnData['data']['id'] + '" />';
+			content +=				'<input type="text" name="lastName' + returnData['data']['id'] + '" id="lastName' + returnData['data']['id'] + '" value="' + returnData['data']['last_name'] + '" />';
 			
 			content += 				'<label>Phone:</label>';
 			content +=				'<div class="phone inputGroup">';
-			content +=					'<input type="text" name="phone1' + returnData['data']['id'] + '" id="phone1' + returnData['data']['id'] + '" />';
-			content +=					'<input type="text" name="phone2' + returnData['data']['id'] + '" id="phone2' + returnData['data']['id'] + '" />';
-			content +=					'<input type="text" name="phone3' + returnData['data']['id'] + '" id="phone3' + returnData['data']['id'] + '" />';
+			content +=					'<input type="text" name="phone1' + returnData['data']['id'] + '" id="phone1' + returnData['data']['id'] + '" value="' + returnData['data']['phone_one'] + '" />';
+			content +=					'<input type="text" name="phone2' + returnData['data']['id'] + '" id="phone2' + returnData['data']['id'] + '" value="' + returnData['data']['phone_two'] + '" />';
+			content +=					'<input type="text" name="phone3' + returnData['data']['id'] + '" id="phone3' + returnData['data']['id'] + '" value="' + returnData['data']['phone_three'] + '" />';
 			content +=				'</div>';
 			
 			content +=				'<label>Email:</label>';
-			content +=				'<input type="text" name="email' + returnData['data']['id'] + '" id="email' + returnData['data']['id'] + '" />';
+			content +=				'<input type="text" name="email' + returnData['data']['id'] + '" id="email' + returnData['data']['id'] + '" value="' + returnData['data']['email'] + '" />';
 			
 			content +=				'<label>Company:</label>';
-			content +=				'<input type="text" name="company' + returnData['data']['id'] + '" id="company' + returnData['data']['id'] + '" />';
+			content +=				'<input type="text" name="company' + returnData['data']['id'] + '" id="company' + returnData['data']['id'] + '" value="' + returnData['data']['company'] + '" />';
 			
 			content +=				'<label>Address:</label>';
 			content +=				'<div class="address inputGroup' + returnData['data']['id'] + '" id="inputGroup' + returnData['data']['id'] + '">';
 			
-			content +=					'<input type="text" name="address1' + returnData['data']['id'] + '" id="address1' + returnData['data']['id'] + '" />';
-			content +=					'<input type="text" name="address2' + returnData['data']['id'] + '" id="address2' + returnData['data']['id'] + '" />';
+			content +=					'<input type="text" name="address1' + returnData['data']['id'] + '" id="address1' + returnData['data']['id'] + '" value="' + returnData['data']['address_one'] + '" />';
+			content +=					'<input type="text" name="address2' + returnData['data']['id'] + '" id="address2' + returnData['data']['id'] + '" value="' + returnData['data']['address_two'] + '" />';
 			content +=				'</div>';
 									
 			content +=				'<div class="areaInfo inputGroup">';
 			content +=					'<label>City:</label>';
-			content +=					'<input type="text" name="city' + returnData['data']['id'] + '" id="city' + returnData['data']['id'] + '" />';
+			content +=					'<input type="text" name="city' + returnData['data']['id'] + '" id="city' + returnData['data']['id'] + '" value="' + returnData['data']['city'] + '" />';
 			
 			content +=					'<label>State:</label>';
 			content +=					'<select name="state" id="state">';
@@ -54,11 +54,11 @@ function Processing() {
 			content +=					'</select>';
 			
 			content +=					'<label>Zip:</label>';
-			content +=					'<input type="text" name="zipCode' + returnData['data']['id'] + '" id="zipCode' + returnData['data']['id'] + '" />';
+			content +=					'<input type="text" name="zipCode' + returnData['data']['id'] + '" id="zipCode' + returnData['data']['id'] + '" value="' + returnData['data']['zip_code'] + '" />';
 			content +=				'</div>';
 			
 			content +=				'<label>Notes:</label>';
-			content +=				'<textarea name="notes' + returnData['data']['id'] + '" id="notes' + returnData['data']['id'] + '"></textarea>';
+			content +=				'<textarea name="notes' + returnData['data']['id'] + '" id="notes' + returnData['data']['id'] + '">' + returnData['data']['notes'] + '</textarea>';
 			
 			content +=				'<button type="submit">Submit</button>';
 			content +=			'</fieldset>';
