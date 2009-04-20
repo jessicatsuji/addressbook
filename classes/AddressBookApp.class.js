3ca5b822fb92e6e37ab3818c6279e08c8573bf6b
Jessica Tsuji <jessicat@Jessica-Tsujis-PowerBook.local>
2009-04-20 09:32:05 -0700

    Set the editing & collapsing features to all contacts initially on page load

--- classes/AddressBookApp.class.js
+++ classes/AddressBookApp.class.js
@@ -8,7 +8,40 @@ function AddressBookApp() {
 		self.root = element;
 		self.interfaceActions.construct(self.root);
 		
-		//self.interfaceActions.addContact();
-		//self.interfaceActions.editContact();
+		self.setContactEdit();
+		
+		this.setContactHide();
+	}
+	
+	this.setContactEdit = function() {
+		$('#interfaceContent form').each(function() {
+			self.sendData = $(this).serialize();
+			self.script = "editContact";
+	
+			$(this).bind('submit', function(){
+				//alert('update widget #' + returnData['data']['id']);
+				self.ajaxAmbassador = new AjaxAmbassador();
+				self.ajaxAmbassador.makeRequest( self.sendData, self.script );
+				return false;
+			});
+		});
+	}
+	
+	this.setContactHide = function() {
+		$('#interfaceContent .contactWidget').each(function() {
+			$("form", this).hide();
+			$(".collapse",this).toggle(function() {
+				var widget = $(this).parent();
+				$('.contact', widget).hide();
+				$('form', widget).fadeIn("fast");
+				$(this).text("Collapse");
+			},
+			function() {
+				var widget = $(this).parent();
+				$('form', widget).hide();
+				$('.contact', widget).fadeIn("fast");
+				$(this).text("Edit");
+			});
+		});
 	}
 }
