6a1f8b50eacff64e4c825fac2bae01de5d482e27
Kellan Craddock <kellan@Kellan-Craddocks-PowerBook-2.local>
2009-04-19 18:15:43 -0700

    Ambassador now uses a switch statement to determine which processing method to use

--- classes/AjaxAmbassador.class.js
+++ classes/AjaxAmbassador.class.js
@@ -1,55 +1,57 @@
 function AjaxAmbassador() {
-  var self = this;
-  this.root;
-  this.sendData;
-  this.script;
-  
-  this.construct = function() {
-  }
-  
-  this.makeRequest = function(sendData, script) {
-  	self.sendData = sendData; 
-    self.script = script;
-    
-    $.ajax({
-      async: true, // default
-      beforeSend: function() { self.preloader(); },
-      cache: true, // default
-      complete: function(returnData, textStatus) { self.complete(returnData, textStatus); },
-      contentType: 'application/x-www-form-urlencoded', //default
-      data: self.sendData, 
-      dataFilter: null, //default
-      dataType: 'json',
-      error: function(returnData, textStatus, errorThrown) { alert("SendMessage Ajax broken: " + textStatus) },
-      global: true, //default
-      ifModified: false, //default
-      success: function(returnData, textStatus) { self.success(returnData, textStatus); },
-      timeout: 10000, // milliseconds
-      url: 'scripts/' + self.script + '.php',
-      type: 'post'
-      
-    });
- 
-  }
-  
-  this.preloader = function() {
-    $('#preloader').html("<img src='images/ajax-loader.gif'/>Loading");
-  }
-  
-  this.success = function(returnData, textStatus) {
-    //Set data variable to be returned
-    return returnData;
-  }
-  
-  this.complete = function(data, textStatus) {
-    $('#preloader').html("");
-  }
-  
-  this.handleData = function(data) {
-    //soundManager.play('correct');
-    
-    this.renderMessages = new RenderMessages();
-    this.renderMessages.construct(data['message']['new']['id'], data['message']['new']['sender'], data['message']['new']['message'], data['message']['new']['time']);
- 
-  }
+	var self = this;
+	this.root;
+	this.sendData;
+	this.script;
+	
+	this.processing = new Processing();
+	
+	this.construct = function() {
+	}
+	
+	//Ajax call to add a contact
+	this.makeRequest = function(sendData, script) {
+		self.sendData = sendData; 
+		self.script = script;
+		
+		$.ajax({
+		  async: true, // default
+		  //beforeSend: function() { self.preloader(); },
+		  cache: true, // default
+		  complete: function(returnData, textStatus) {},
+		  contentType: 'application/x-www-form-urlencoded', //default
+		  data: self.sendData, 
+		  dataFilter: null, //default
+		  dataType: 'json',
+		  error: function(returnData, textStatus, errorThrown) { alert("SendMessage Ajax broken: " + textStatus);  },
+		  global: true, //default
+		  ifModified: false, //default
+		  success: function(returnData, textStatus) { self.switchProcessing(returnData); },
+		  timeout: 10000, // milliseconds
+		  url: 'scripts/' + self.script + '.php',
+		  type: 'post'
+		  
+		});
+	}
+	
+	this.switchProcessing = function(returnData) {
+		switch(self.script) {
+			case "addContact":
+				self.processing.addContact(returnData);
+				break;
+			case "editContact":
+				self.processing.editContact(returnData);
+				break;
+				default:
+				break;
+		}
+	}
+	
+	this.preloader = function() {
+		$('#preloader').html("<img src='images/ajax-loader.gif'/>Loading");
+	}
+	
+	this.complete = function(returnData, textStatus) {
+	// $('#preloader').html("");
+	}
 }
