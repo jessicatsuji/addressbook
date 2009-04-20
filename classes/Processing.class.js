e85455c8c3c3897d23341a36fa782ae3fb62eba7
Gerardo Rodriguez <chocho@chocho.local>
2009-04-19 21:16:27 -0700

    Added the content that will create the widget divs

--- classes/Processing.class.js
+++ classes/Processing.class.js
@@ -11,6 +11,61 @@ function Processing() {
 			self.writeErrors(returnData);
 		} else {
 			alert('ID: ' + returnData['id'] + ' added!');
+			alert('data: ' + returnData['data']['zip_code']);
+			
+			var content;
+			
+			content = 	'<div id="addContact">';
+			content += 		'<form action="#" method="post">';
+			content += 			'<fieldset>';
+			content += 				'<label>First Name:</label>';
+			content += 				'<input type="text" name="firstName" />';
+			
+			content += 				'<label>Last Name:</label>';
+			content +=				'<input type="text" name="lastName" />';
+			
+			content += 				'<label>Phone:</label>';
+			content +=				'<div class="phone inputGroup">';
+			content +=					'<input type="text" name="phone1" />';
+			content +=					'<input type="text" name="phone2" />';
+			content +=					'<input type="text" name="phone3" />';
+			content +=				'</div>';
+			
+			content +=				'<label>Email:</label>';
+			content +=				'<input type="text" name="email" />';
+			
+			content +=				'<label>Company:</label>';
+			content +=				'<input type="text" name="company" />';
+			
+			content +=				'<label>Address:</label>';
+			content +=				'<div class="address inputGroup">';
+			
+			content +=					'<input type="text" name="address1" />';
+			content +=					'<input type="text" name="address2" />';
+			content +=				'</div>';
+									
+			content +=				'<div class="areaInfo inputGroup">';
+			content +=					'<label>City:</label>';
+			content +=					'<input type="text" name="city" />';
+			
+			content +=					'<label>State:</label>';
+			content +=					'<select name="state" id="state">';
+			content +=						'<option label="OR" title="Oregon" value="1">OR</option>';
+			content +=					'</select>';
+			
+			content +=					'<label>Zip:</label>';
+			content +=					'<input type="text" name="zipCode" />';
+			content +=				'</div>';
+			
+			content +=				'<label>Notes:</label>';
+			content +=				'<textarea name="notes"></textarea>';
+			
+			content +=				'<button type="submit">Submit</button>';
+			content +=			'</fieldset>';
+			content +=		'</form>';
+			content +=	'</div>';
+			
+			$('#interfaceContent', self.root).append( content );
 		}	
 	}
 	
