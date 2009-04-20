e2ace65c7e3b0f1072e58b5d58bf697fc1461dc4
Jessica Tsuji <jessicat@Jessica-Tsujis-PowerBook.local>
2009-04-20 10:20:04 -0700

    moved setContactEdit & setContactHide methods to InterfaceActions class instead of AddressBookApp class

--- classes/AddressBookApp.class.js
+++ classes/AddressBookApp.class.js
@@ -7,41 +7,5 @@ function AddressBookApp() {
 	this.construct = function(element) {
 		self.root = element;
 		self.interfaceActions.construct(self.root);
-		
-		self.setContactEdit();
-		
-		this.setContactHide();
-	}
-	
-	this.setContactEdit = function() {
-		$('#interfaceContent form').each(function() {
-			self.sendData = $(this).serialize();
-			self.script = "editContact";
-	
-			$(this).bind('submit', function(){
-				//alert('update widget #' + returnData['data']['id']);
-				self.ajaxAmbassador = new AjaxAmbassador();
-				self.ajaxAmbassador.makeRequest( self.sendData, self.script );
-				return false;
-			});
-		});
-	}
-	
-	this.setContactHide = function() {
-		$('#interfaceContent .contactWidget').each(function() {
-			$("form", this).hide();
-			$(".collapse",this).toggle(function() {
-				var widget = $(this).parent();
-				$('.contact', widget).hide();
-				$('form', widget).fadeIn("fast");
-				$(this).text("Collapse");
-			},
-			function() {
-				var widget = $(this).parent();
-				$('form', widget).hide();
-				$('.contact', widget).fadeIn("fast");
-				$(this).text("Edit");
-			});
-		});
 	}
 }
