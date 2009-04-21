b84a2936d5159b4d742fa237806cb57f265beb53
Jessica Tsuji <jessicat@Jessica-Tsujis-PowerBook.local>
2009-04-20 19:02:36 -0700

    Attempting JsDocs

--- classes/InterfaceActions.class.js
+++ classes/InterfaceActions.class.js
@@ -1,28 +1,66 @@
-function InterfaceActions() {
+/**
+ * Creates a InterfaceActions binds all events to HTML elements for UI & instansiates the AjaxAmbassador class for functionality.
+ * @class InterfaceActions Handles user events, hide/show the panel to add a new contact, hide/show mini info contact widget & the editing contact info form, & handles adding & editing methods.
+ * @version 1.0
+ * 
+ * @property {Object} self Internal reference to <code>this</code>.
+ * @property {Object} root jQuery Dom Reference to root node for instance.
+ * 
+ */
+ 
+ function InterfaceActions() {
 	var self = this;
 	this.root;
-	//Create new instance of ambassador class here
 	
+	/**
+	 * Creates an instance of the AjaxAmbassador class to be used to handle firing ajax & handling ajax response.
+	 * @see AjaxAmbassador
+	 * @returns {Object} Returns an instance of AjaxAmbassador object.
+	 */
 	this.ajaxAmbassador = new AjaxAmbassador();
 	
+	/**
+	 * @constructor
+	 * @param {Object} element The $('#wrapper') jQuery DOM reference, containing all adding forms, editing forms, & rest of HTML structure.
+	 */
 	this.construct = function(element) {
 		//construct ambassador class
 		self.root = element;
 		
+		/**
+		 * Fires off constructor for AjaxAmbassador object
+		 * @param {Object} element The $('#wrapper') jQuery DOM reference, containing all adding forms, editing forms, & rest of HTML structure.
+		 * @returns {null} Does not return a value.
+		 */
 		self.ajaxAmbassador.construct(element);
 		
-		//Hide/Show add contact panel
+		/**
+		 * Fires off internal method #showAddPanel to show/hide the panel used for adding a new contact.
+		 * @param {Object} element The $('#wrapper') jQuery DOM reference, containing all adding forms, editing forms, & rest of HTML structure.
+		 * @returns {null} Does not return a value.
+		 */
 		self.showAddPanel(element);
 		
+		/**
+		 * Binds a submit event to the form in the panel used to add a new contact & fires off internal method #addContact
+		 */
 		$('#addContactForm', element).bind('submit', function() {
 			self.addContact(self.root);
 			return false;
 		});
 		
+		/**
+		 * Fires off internal method #setContactHide for each .contactWidget element in the #interfaceContent area.
+		 * @returns {boolean} false.
+		 */
 		$('#interfaceContent .contactWidget').each(function() {
 			self.setContactHide(this);
 		});
 		
+		/**
+		 * Binds a submit event for each form element in the #interfaceContent area to fire off the internal method #editContact.
+		 * @returns {boolean} false.
+		 */
 		$('#interfaceContent form').each(function() {
 			$(this).bind('submit', function(){
 				self.editContact(this);
@@ -31,6 +69,11 @@ function InterfaceActions() {
 		});
 	}
 	
+	/**
+	 * Binds the events to show & hide & toggle the panel used for adding a new contact.
+	 * @member InterfaceActions#showAddPanel
+	 * @param {Object} element The $('#wrapper') jQuery DOM reference, containing all adding forms, editing forms, & rest of HTML structure.
+	 */
 	this.showAddPanel = function(element) {
 		var addBtn = $("#addBtn", element);
 		var addPanel = $("#addContact", element);
